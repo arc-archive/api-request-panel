b2b23d5703bc6819dbe9938aedf53a12e374a7c2
Pawel Psztyc <jarrodek@gmail.com>
2018-04-27 18:22:04 -0700

    Updating AMF + adding new model.

--- demo/model.js
+++ demo/model.js
@@ -9,25 +9,32 @@ const files = new Map();
 files.set('demo-api/demo-api.raml', 'RAML 1.0');
 files.set('array-body/array-body.raml', 'RAML 1.0');
 files.set('appian-api/appian-api.raml', 'RAML 1.0');
-// files.set('github-api/github-api.raml', 'RAML 0.8');
 files.set('nexmo-sms-api/nexmo-sms-api.raml', 'RAML 1.0');
+files.set(`loan-ms/loan-microservice.json`, 'OAS 2.0');
 /**
  * Generates json/ld file from parsed document.
  *
  * @param {Object} doc
  * @param {String} file
+ * @param {String} type type of source document
  * @return {Promise}
  */
-function processFile(doc, file) {
+function processFile(doc, file, type) {
   const generator = amf.Core.generator('AMF Graph', 'application/ld+json');
-  const r = amf.Core.resolver('RAML 1.0');
+  console.log('Resolving', file);
+  const r = amf.Core.resolver(type);
   doc = r.resolve(doc, 'editing');
   let dest = file.substr(0, file.lastIndexOf('.')) + '.json';
   if (dest.indexOf('/') !== -1) {
     dest = dest.substr(dest.lastIndexOf('/'));
   }
+  console.log('Generating data', file);
+  let genTime = Date.now();
   return generator.generateString(doc)
-  .then((data) => fs.writeFileSync('demo/' + dest, data, 'utf8'));
+  .then((data) => {
+    console.log('Generation took %d ms for %s', (Date.now() - genTime), file);
+    fs.writeFileSync('demo/' + dest, data, 'utf8');
+  });
 }
 /**
  * Parses file and sends it to process.
@@ -37,9 +44,10 @@ function processFile(doc, file) {
  * @return {String}
  */
 function parseFile(file, type) {
+  console.log('Parsing', file);
   const parser = amf.Core.parser(type, 'application/yaml');
   return parser.parseFileAsync(`file://demo/${file}`)
-  .then((doc) => processFile(doc, file));
+  .then((doc) => processFile(doc, file, type));
 }
 
 amf.Core.init().then(() => {
